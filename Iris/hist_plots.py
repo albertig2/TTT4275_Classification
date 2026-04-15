d3c39726918ea994bbfa4ec563ee0967ff95fb4f
TheGoat <birkbones@gmail.com>
2026-04-15 16:04:05 +0200

    Gjorde ferdig oppgave 1

---
+++ Iris/hist_plots.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+c1 = np.loadtxt("Iris/class_1", delimiter=",")
+c2 = np.loadtxt("Iris/class_2", delimiter=",")
+c3 = np.loadtxt("Iris/class_3", delimiter=",")
+
+print(np.shape(c1))
+DIM = 4
+
+for i in range(DIM):
+    plt.hist(c1[:, i], alpha=0.5, label="Class 1")
+    plt.hist(c2[:, i], alpha = 0.5, label="Class 2")
+    plt.hist(c3[:, i], alpha = 0.5, label="Class 3")
+    plt.title("Feature " + str(i))
+    plt.legend()
+    plt.savefig("Plots/Feature " + str(i))
+    plt.cla()
+
