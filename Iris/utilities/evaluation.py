3752e3a3bce68cc6c8d3e062fd29f0108a6087a6
Albertine <albertine.gjos@orbitntnu.com>
2026-04-17 09:58:20 +0200

    +1

--- Iris/utilities/evaluation.py
+++ Iris/utilities/evaluation.py
@@ -8,9 +8,9 @@ def generate_histograms_for_fetures(class1_features, class2_features, class3_fea
         plt.hist(class1_features[:, i], alpha=0.5, label="Class 1")
         plt.hist(class2_features[:, i], alpha = 0.5, label="Class 2")
         plt.hist(class3_features[:, i], alpha = 0.5, label="Class 3")
-        plt.title("Feature " + str(i))
+        plt.title("Feature " + str(i + 1))
         plt.legend()
-        plt.savefig(join(dirname(dirname(__file__)), 'results/Feature_' + str(i) + ".png"))
+        plt.savefig(join(dirname(dirname(__file__)), 'results/Feature_' + str(i + 1) + ".png"))
         plt.cla()
         
         
